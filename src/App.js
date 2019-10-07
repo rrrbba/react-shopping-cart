43303a0bc1337eff85001a689722090f02f2fe14
Rayven <rayven.burns@gmail.com>
2019-10-07 18:35:56 -0400

    mvp done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
@@ -18,24 +20,28 @@ function App() {
 
 	return (
 		<div className="App">
+
+			{/* The below allows us to add books to the cart */}
+			<ProductContext.Provider value = {{ products, addItem }}>
+			<CartContext.Provider value = {cart}>
+
 			<Navigation cart={cart} />
 
 			{/* Routes */}
 			<Route
 				exact
 				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
+				component = {Products}
+			
 			/>
 
 			<Route
 				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
+				component = {ShoppingCart} />
 			/>
+
+			</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
